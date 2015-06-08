083b9ed2f4baf86442e635d9241a6e38cce4d9f5
samuelpampolini@gmail.com <samuelpampolini@gmail.com>
2015-06-08 14:42:10 -0300

    Create the first api controller.

---
+++ MovieReview.Web/MovieReview.Web/Controllers/ApiBaseController.cs
@@ -0,0 +1,15 @@
+﻿using MovieReview.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MovieReview.Web.Controllers
+{
+    public class ApiBaseController : ApiController
+    {
+        protected IMovieReviewUow Uow { get; set; }
+    }
+}
---
+++ MovieReview.Web/MovieReview.Web/Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using MovieReview.Data.Contracts;
+using MovieReview.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieReview.Web.Controllers
+{
+    public class MoviesController : ApiBaseController
+    {
+        public MoviesController(IMovieReviewUow uow)
+        {
+            Uow = uow;
+        }
+
+        // GET: api/Movies
+        public IEnumerable<Movie> Get()
+        {
+            return Uow.Movies.GetAll().OrderBy(s => s.MovieName);
+        }
+
+        // GET: api/Movies/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Movies
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Movies/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Movies/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
