823df45d35e592a5d9c5894b251fa36932fc5c40
samuelpampolini@gmail.com <samuelpampolini@gmail.com>
2015-06-25 15:33:34 -0300

    Mock de dados

--- MovieReview.Web/MoviewReview.Tests/ClientTests/homeIndexTest.js
+++ MovieReview.Web/MoviewReview.Tests/ClientTests/homeIndexTest.js
@@ -18,4 +18,64 @@ describe("home-Index Tests-->", function () {
             expect(dataServiceFactory.movies.length).toEqual(0);
         }));
     });
+
+    var $httpBackend;
+    var url = "/api/movies";
+
+    var fakedMoviesResponse = [{
+        Id: 1,
+        MovieName: "Godzilla",
+        DirectorName: "Gareth Edwards",
+        ReleaseYear: "2014",
+        NoOfReviews: 6
+    },
+    {
+        Id: 3,
+        MovieName: "Titanic",
+        DirectorName: "James Cameron",
+        ReleaseYear: "1997",
+        NoOfReviews: 3
+    },
+    {
+        Id: 4,
+        MovieName: "Die Another Day",
+        DirectorName: "Lee Tamahori",
+        ReleaseYear: "2002",
+        NoOfReviews: 0
+    },
+    {
+        Id: 7,
+        MovieName: "Taken 3",
+        DirectorName: "Olivier Megaton",
+        ReleaseYear: "2014",
+        NoOfReviews: 0
+    },
+    {
+        Id: 9,
+        MovieName: "Top Gun",
+        DirectorName: "Tony Scott",
+        ReleaseYear: "1986",
+        NoOfReviews: 0
+    }];
+
+    beforeEach(inject(function ($injector) {
+        $httpBackend = $injector.get("$httpBackend");
+        $httpBackend.whenGET(url)
+            .respond(fakedMoviesResponse);
+    }));
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+    //test the backend call
+
+    describe("Testing Movies GET Call-->", function () {
+        it("Loaded Movies", inject(function (dataServiceFactory) {
+            $httpBackend.expectGET(url);
+            dataServiceFactory.getMovies();
+            $httpBackend.flush();
+            expect(dataServiceFactory.movies.length).toEqual(5);
+        }));
+    });
 });
