9d28ca48c0ab64d2f3be66875d3e744e6d069496
samuelpampolini@gmail.com <samuelpampolini@gmail.com>
2015-06-25 15:14:14 -0300

    Primeiro teste com o Angular

---
+++ MovieReview.Web/MoviewReview.Tests/ClientTests/homeIndexTest.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../scripts/jasmine.js" />
+/// <reference path="../../moviereview.web/scripts/angular.min.js" />
+/// <reference path="../../moviereview.web/scripts/angular-ui-bootstrap.min.js" />
+/// <reference path="../../moviereview.web/scripts/angular-route.min.js" />
+/// <reference path="../../moviereview.web/scripts/angular-mocks.js" />
+/// <reference path="../../moviereview.web/js/homeindex.js" />
+/// <reference path="../../moviereview.web/js/movie-review-edit.js" />
+
+describe("home-Index Tests-->", function () {
+
+    beforeEach(angular.mock.module('homeIndex'));
+
+    //to test individual bits and bytes inside the home-Index
+    describe("dataService-->", function () {
+
+        it("can load movies", inject(function (dataServiceFactory) {
+            //for the 1st Run
+            expect(dataServiceFactory.movies.length).toEqual(0);
+        }));
+    });
+});
